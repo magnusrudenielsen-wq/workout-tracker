00def4a159f68906b96b551ee42cc7fa64d78ef7
magnusrudenielsen-wq <magnusrudenielsen@gmail.com>
2026-05-04 22:18:48 +0200

    Initial workout tracker

---
+++ Sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'workout-v1';
+const ASSETS = [
+  '/workout-tracker/',
+  '/workout-tracker/index.html',
+  '/workout-tracker/manifest.json',
+  'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=DM+Sans:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
